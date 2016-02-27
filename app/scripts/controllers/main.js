16895ae5d0106ea849c037c336c95c0f576622f7
Aaron Turner <aaron@aaronthedev.com>
2016-02-27 02:08:17 -0800

    Integrated wad and tested working
    Currently have some test values, but glad to see everything working

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -4,5 +4,23 @@
   angular.module('Home', []).
   controller('MainController', ['$scope', MainController]);
 
-  function MainController($scope) {}
+  function MainController($scope) {
+
+      //Put logic here
+
+      //Play a sound
+      $scope.playSong = function() {
+          var bell = new Wad({
+              source : 'file:///home/aaron/Music/test/song1.mp3',
+              env : { hold : 100000000 },
+              delay   : {
+                delayTime : .5,  // Time in seconds between each delayed playback.
+                wet       : .25, // Relative volume change between the original sound and the first delayed playback.
+                feedback  : .25, // Relative volume change between each delayed playback and the next.
+            }
+          });
+            bell.play();
+            console.log(bell);
+      }
+  }
 })();
