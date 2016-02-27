04e71e3d495186bfc591bbedabe9c33771ca3d08
Julian Poyourow <julianpoyo@gmail.com>
2016-02-27 04:09:22 -0800

    Finished the entirity of multi song loading
    Got highlighting working and everything.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -6,36 +6,40 @@
 
   function MainController($scope) {
 
+      var path = require('path');
+
       //Put logic here
       var remote = require('remote');
       var dialog = remote.require('dialog');
 
-      //Our file
-      $scope.trackOneFileUrl;
-
+      //Our files
       $scope.tracks = [];
 
       //Open a file
       $scope.openFile = function() {
           dialog.showOpenDialog({ properties: [ 'openFile']}, function(fileName) {
+              fileName = String(fileName);
               //Check if fileName is a file or directory.
-              var isFile = str.match(/.*\..../g);
+              var isFile = fileName.match(/.*\..../g);
               if(isFile){
-                $scope.tracks.push(fileName);
+                var title = path.basename(fileName);
+                $scope.tracks.push({"uri": fileName, "title": title});
               } else {
-                var dir = fs.readdirSync(path);
+                var dir = fs.readdirSync(fileName);
                 for(var i=0;i<dir.length;i++){
-                  $scope.tracks.push(dir[i]);
+                  var title = path.basename(dir[i]);
+                  $scope.tracks.push({"uri": dir[i], "title": title});
                 }
               }
+              $scope.$apply();
           });
       }
 
       //Play a sound
-      $scope.playSong = function() {
-
-          $scope.trackOne = new Wad({
-              source : 'file://' + $scope.trackOneFileUrl,
+      $scope.playSong = function(index) {
+          console.log($scope.tracks);
+          $scope.tracks[index].player = new Wad({
+              source : 'file://' + $scope.tracks[index].uri,
               env : { hold : 100000000 },
               delay   : {
                 delayTime : 0,  // Time in seconds between each delayed playback.
@@ -43,7 +47,8 @@
                 feedback  : .25, // Relative volume change between each delayed playback and the next.
             }
           });
-            $scope.trackOne.play();
+          $scope.tracks[index].player.play();
+          $scope.tracks[index].playing = true;
       }
   }
 })();
