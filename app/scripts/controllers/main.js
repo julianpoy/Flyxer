9ae061694386efb36e110876d0faf2d1819b6f72
Julian Poyourow <julianpoyo@gmail.com>
2016-02-27 05:38:55 -0800

    Added final folder importer

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,6 +7,7 @@
   function MainController($scope) {
 
       var path = require('path');
+      var fs = require('fs');
 
       //Put logic here
       var remote = require('remote');
@@ -17,7 +18,7 @@
 
       //Open a file
       $scope.openFile = function() {
-          dialog.showOpenDialog({ properties: [ 'openFile']}, function(fileName) {
+          dialog.showOpenDialog({ properties: [ 'openFile', 'openDirectory' ]}, function(fileName) {
               fileName = String(fileName);
               //Check if fileName is a file or directory.
               var isFile = fileName.match(/.*\..../g);
@@ -27,28 +28,42 @@
               } else {
                 var dir = fs.readdirSync(fileName);
                 for(var i=0;i<dir.length;i++){
+                  var forwardSlash = fileName.match(/\//g);
+                  var fullPath;
+                  if(forwardSlash){
+                    var fullPath = fileName + "/" + String(dir[i]);
+                  } else {
+                    var fullPath = fileName + "\\" + String(dir[i]);
+                  }
                   var title = path.basename(dir[i]);
-                  $scope.tracks.push({"uri": dir[i], "title": title});
+                  $scope.tracks.push({"uri": fullPath, "title": title});
                 }
               }
               $scope.$apply();
           });
       }
 
-      //Play a sound
-      $scope.playSong = function(index) {
-          console.log($scope.tracks);
-          $scope.tracks[index].player = new Wad({
-              source : 'file://' + $scope.tracks[index].uri,
-              env : { hold : 100000000 },
-              delay   : {
-                delayTime : 0,  // Time in seconds between each delayed playback.
-                wet       : .25, // Relative volume change between the original sound and the first delayed playback.
-                feedback  : .25, // Relative volume change between each delayed playback and the next.
-            }
-          });
-          $scope.tracks[index].player.play();
-          $scope.tracks[index].playing = true;
+      //Play/stop a track
+      $scope.toggleTrack = function(index) {
+          //Check if track is currently playing
+          if($scope.tracks[index].playing){
+            //Stop song playing
+            $scope.tracks[index].player.stop();
+            $scope.tracks[index].playing = false;
+            $scope.tracks[index].player = null;
+          } else {
+            $scope.tracks[index].player = new Wad({
+                source : 'file://' + $scope.tracks[index].uri,
+                env : { hold : 100000000 },
+                delay   : {
+                  delayTime : 0,  // Time in seconds between each delayed playback.
+                  wet       : .25, // Relative volume change between the original sound and the first delayed playback.
+                  feedback  : .25, // Relative volume change between each delayed playback and the next.
+              }
+            });
+            $scope.tracks[index].player.play();
+            $scope.tracks[index].playing = true;
+          }
       }
   }
 })();
